3d3a626b959293758d819efecbeebcf9c91e66b6
Luigi213 <reyesgianluigi88@gmail.com>
2022-11-30 14:54:49 +0100

    done the bonus mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,19 +1,23 @@
-// 1- Inserire email
-// 1.1 uso prompt per chiedere email
-let email = prompt("Inserire la email");
-// 2- Creo lista di chi può entrare
-let array_list = ["Alessandro@gmail.com", "Marco@gmail.com", "Alessio@gmail.com"];
-// 2.1 controllo nella lista se la email inserita può essere accettata
-let accepted = false
-for(let i=0; i<array_list.length; i++){
-    if(array_list[i] == email){
-       accepted = true 
+let button = document.querySelector("#btn");
+button.addEventListener('click', function(){
+    // 1- Inserire email
+    // 1.1 uso prompt per chiedere email
+    let email = document.getElementById("mail").value;
+    // 2- Creo lista di chi può entrare
+    let array_list = ["Alessandro@gmail.com", "Marco@gmail.com", "Alessio@gmail.com"];
+    // 2.1 controllo nella lista se la email inserita può essere accettata
+    let accepted = false
+    for(let i=0; i<array_list.length; i++){
+        if(array_list[i] == email){
+           accepted = true 
+        }
     }
-}
-// 3- stampo l'esit0
-if(accepted){
-    console.log('Email confermata')
-}
-else{
-    console.log('Email Respista')
-}
+    // 3- stampo l'esito;
+    if(accepted){
+        result = 'Email confermata'
+    }
+    else{
+        result = 'Email errata'
+    }
+    document.getElementById("result").innerHTML = result
+})
--- mail/readme.txt
+++ mail/readme.txt
@@ -4,4 +4,4 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
     // 1.1 uso prompt per chiedere email
 // 2- Creo lista di chi può entrare
     // 2.1 controllo nella lista se la email inserita può essere accettata
-// 3- stampo l'esit0
+// 3- stampo l'esito
